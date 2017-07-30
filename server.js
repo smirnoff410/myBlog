d2788cc44ae4938880a36a8121c22649bd48d5ed
Vladislav <vladislav@vladislav-VirtualBox.(none)>
2017-07-30 18:32:14 +0400

    Добавил осмысленный текст в About page и поигрался с цветами

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var app = express();
 var jsonParser = bodyParser.json();
 var url = "mongodb://localhost:27017/regexp";
 
-var sms=[];
-
 app.use(express.static(__dirname + "/public"));
 
 app.listen(3000, function(){
